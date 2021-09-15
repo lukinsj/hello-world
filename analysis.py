872521000497340f608c1e6cbb0dbbe4bb745149
Jerry Lukins <lukinsj@yahoo.com>
2021-09-15 16:34:44 -0400

    Exercise 1

--- analysis.py
+++ analysis.py
@@ -1,5 +1,23 @@
 with open("SAFI_results.csv") as file:
     file.readline().split(",")[18]
-    for line in file:
-        print(line.split(",")[18]) #index 18
 
+    n_grass = 0 
+    n_mabatipitched = 0
+    n_mabatisloping = 0
+    n_unknown = 0
+
+    for line in file:
+        roof_type = line.split(",")[18] #index 18, the 19th column is C01_respondent_roof_type
+        if roof_type == "grass":
+            n_grass = n_grass + 1
+        elif roof_type == "mabatipitched":
+            n_mabatipitched += 1
+        elif roof_type == "mabatisloping":
+            n_mabatisloping += 1
+        else:
+            n_unknown += 1
+
+    print("There are " + str(n_grass) + " grass roofs")
+    print("There are " + str(n_mabatipitched) + " mabatipitched roofs")
+    print("There are " + str(n_mabatisloping) + " mabatisloping roofs")
+    print("There are " + str(n_unknown) + " unknown roof types")
